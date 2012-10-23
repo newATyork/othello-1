e830d2b00659fc889c70361b02d22011143b72c2
evanandrewrose <evanandrewrose@gmail.com>
2012-10-23 17:02:31 -0400

    Added broken functionality

--- Board.py
+++ Board.py
@@ -28,10 +28,16 @@ class Board:
         ]
 
     def flip_piece(self, y, x):
+        visual_x = y
+        visual_y = x
         x -= 1
         y -= 1
-        if (self.grid[x, y] == 'B'):
-            raise Exception
+        if (self.grid[x][y] == 'X'):
+            self.grid[x][y] = 'O'
+        elif (self.grid[x][y] == 'O'):
+            self.grid[x][y] = 'X'
+        elif (self.grid[x][y] == 'B'):
+            raise Exception("Trying to flip empty piece (%d, %d)." % (visual_x, visual_y))
 
     def insert_white(self, y, x):
         x -= 1
@@ -49,6 +55,11 @@ class Board:
         else:
             self.grid[x][y] = 'X'
 
+    def get_piece(self, y, x):
+        x -= 1
+        y -= 1
+        return self.grid[x][y]
+
     def display(self):
         print("    [1] [2] [3] [4] [5] [6] [7] [8]\n")
         for index, column in enumerate(self.grid):
@@ -59,11 +70,40 @@ class Board:
             if (index < 7):
                 print("") # skip a lin e
 
-    def valid_move(self, player, x, y):
-        """ Returns true if the player is allowed to make move
+    def make_move(self, player, x, y):
+        """ Alters the board if move is valid.
+        
+        Returns true if the player is allowed to make move, returns
+        false otherwise.
 
-        e.g, if valid_move("O", 3, 2):
-            do_move
+        e.g, if make_move("O", 3, 2):
+            # continue game
 
         """
-        return True
+        # find adjacent pieces of opposite color
+        adjacent_pieces = []
+        valid_move = False
+
+        for i in range(-1,2):
+            for j in range(-1,2):
+                if (i == 0 and j == 0):
+                    pass # don't check the piece itself
+                if x+i < 9 and y+j < 9 and x+i > 0 and y+i > 0:
+                    piece = self.get_piece(x+i, y+j)
+                    if piece != player and piece != "B":
+                        c_x = x+i
+                        c_y = y+j
+                        while c_x < 9 and c_y < 9 and c_x > 0 and c_y > 0:
+                            if (self.get_piece(c_x, c_y) == player):
+                                valid_move = True
+                                f_x = c_x + i
+                                f_y = c_y + j
+                                while self.get_piece(f_x, f_y) != player:
+                                    print("flipping %d, %d" % (f_x, f_y))
+                                    self.flip_piece(f_x, f_y)
+                                    f_x += i
+                                    f_y += j
+                            c_x += i
+                            c_y += j
+
+        return valid_move
--- othello.py
+++ othello.py
@@ -4,19 +4,23 @@ def main():
     b = Board()
     game_over = False
     player_turn = "O"
+
     while not game_over:
         b.display()
         print("It is %s 's turn." % ("white" if player_turn=="O" else "black"))
         x,y = get_input()
         x = int(x)
         y = int(y)
-        if (b.valid_move(player_turn, x, y)):
+
+        if (b.make_move(player_turn, x, y)):
             if (player_turn == "O"):
                 b.insert_white(x, y)
             else:
                 b.insert_black(x, y)
-        player_turn = "X" if player_turn != "X" else "O"
+        else:
+            print("Not valid move.")
 
+        player_turn = "X" if player_turn != "X" else "O"
 
 def get_input():
     print("Give me your input in x,y format (e.g, 5,2): ")
