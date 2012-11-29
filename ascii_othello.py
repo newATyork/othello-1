657454721aff06e1e2dba6826b349d8eb8ab3c81
Evan Rose <erose@linux14.(none)>
2012-11-29 12:52:33 -0500

    Started gui version

--- ascii_othello.py
+++ ascii_othello.py
@@ -1,16 +1,28 @@
 from Board import Board
 import re
-import pygame
 
 def main():
     b = Board()
     game_over = False
     player_turn = "O"
 
-    pygame.init()
-    pygame.display.set_mode((800,600))
+    while not game_over:
+        b.display()
+        print("It is %s 's turn." % ("white" if player_turn=="O" else "black"))
+        x,y = get_input()
+        x = int(x)
+        y = int(y)
+        move_made = False
 
-"""
+        if (b.make_move(player_turn, x, y)):
+            if (player_turn == "O"):
+                b.insert_white(x, y)
+            else:
+                b.insert_black(x, y)
+            move_made = True
+        else:
+            print("Not a valid move. Try again.")
+
         if move_made: # in case of invalid move
             player_turn = "X" if player_turn != "X" else "O"
             if player_turn == "X":
@@ -25,7 +37,6 @@ def main():
                         game_over = True
                     else:
                         print("White has no more moves.")
-"""
 
 def get_input():
     choice = ""
